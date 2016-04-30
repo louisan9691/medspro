f44b6b57edf479aec21c7bdfff89ff3bfb74abbd
Louis An <louisan@louiss-imac.local>
2016-04-30 23:21:24 +1000

    Initial Commit

---
+++ MedsPro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MedsPro
+//
+//  Created by Louis An on 30/04/2016.
+//  Copyright © 2016 Louis An. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
